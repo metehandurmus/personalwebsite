1605fc4d9b0f5a9f7c863f923b405121df6d0302
metehandurmus <031990016@ogr.uludag.edu.tr>
2021-10-31 15:46:27 +0300

    Gece modu eklendi.

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-    include 'main.min.html';
-    if($_POST) {
+    $saat = date("H");
+    if ($saat > 18)
+        include 'main.dark.min.html';
+    else
+        include 'main.dark.min.html';
+
+    if($_POST)
         include 'vt_mailekle.php';
-    } else if ($_GET) {
+    else if ($_GET)
         echo '<div class="alert error animate__animated">Yanlış bir işlem yaptınız.</div>';
-    }
 ?>
